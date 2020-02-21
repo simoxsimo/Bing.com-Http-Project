f377ffe98df0b9b0e321f5ccf642c5be8cc1345c
bafiam <stevebafiam@gmail.com>
2020-02-22 00:01:37 +0300

    add search method and execute the class

--- binghttp.rb
+++ binghttp.rb
@@ -1,7 +1,7 @@
 require 'rest-client'
 
 class Binghttp
-  attr_accessor :query
+  attr_accessor :query, :response
   def initialize()
     @query = ''
   end
@@ -9,13 +9,30 @@ class Binghttp
   def query_transform()
     query_get
     @query = @query.downcase.tr(' ', '+')
+    search_get
   end
 
   def query_get()
     puts 'What are you Searching for?'
     @query = gets.chomp
   end
+
+  def search_get
+    url = "https://www.bing.com/search?q=#{@query}"
+    @response = RestClient.get url
+  end
 end
 
-# search = Binghttp.new
-# puts search.query_transform
+run = Binghttp.new
+results = run.query_transform
+
+# get code
+# puts results.code
+
+# get header
+# puts results.headers
+
+# get cookie
+puts results.cookies
+# get body
+# puts results.body
